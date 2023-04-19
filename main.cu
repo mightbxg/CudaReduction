9e3c8f8ee71f912fc58703fd2599a223b7b30d20
bianxinguang <bianxinguang@baidu.com>
2023-04-19 13:05:45 +0800

    complete

--- main.cu
+++ main.cu
@@ -1,6 +1,7 @@
 #include <cuda_runtime.h>
 #include <curand.h>
 
+#include <cassert>
 #include <iostream>
 #include <sstream>
 #include <vector>
@@ -30,9 +31,63 @@ using namespace std;
     }                                                                              \
   } while (false)
 
-template <unsigned int blockSize>
-__device__ void warpReduce(volatile int* sdata, unsigned int tid) {
-  if (blockSize >= 64) sdata[tid] += sdata[tid + 32];
+template <unsigned int kBlockSize>
+__device__ void warpReduce(volatile float *sdata, unsigned int tid) {
+  if (kBlockSize >= 64) sdata[tid] += sdata[tid + 32];
+  if (kBlockSize >= 32) sdata[tid] += sdata[tid + 16];
+  if (kBlockSize >= 16) sdata[tid] += sdata[tid + 8];
+  if (kBlockSize >= 8) sdata[tid] += sdata[tid + 4];
+  if (kBlockSize >= 4) sdata[tid] += sdata[tid + 2];
+  if (kBlockSize >= 2) sdata[tid] += sdata[tid + 1];
+}
+
+template <unsigned int kBlockSize>
+__global__ void reduce(const float *g_idata, float *g_odata, size_t n) {
+  extern __shared__ float sdata[];
+  const unsigned int tid = threadIdx.x;
+  unsigned int i = blockIdx.x * (kBlockSize * 2) + tid;
+  float my_sum = i < n ? g_idata[i] : 0.0f;
+  if (i + kBlockSize < n) my_sum += g_idata[i + kBlockSize];
+  sdata[tid] = my_sum;
+  __syncthreads();
+
+  if (kBlockSize >= 1024) {
+    if (tid < 512) sdata[tid] += sdata[tid + 512];
+    __syncthreads();
+  }
+  if (kBlockSize >= 512) {
+    if (tid < 256) sdata[tid] += sdata[tid + 256];
+    __syncthreads();
+  }
+  if (kBlockSize >= 256) {
+    if (tid < 128) sdata[tid] += sdata[tid + 128];
+    __syncthreads();
+  }
+  if (kBlockSize >= 128) {
+    if (tid < 64) sdata[tid] += sdata[tid + 64];
+    __syncthreads();
+  }
+  if (tid < 32) warpReduce<kBlockSize>(sdata, tid);
+  if (tid == 0) g_odata[blockIdx.x] = sdata[0];
+}
+
+template <unsigned kThreadNum>
+float reduce_cu(const float *g_idata, int num) {
+  const unsigned int blk_num = (num + kThreadNum * 2 - 1) / (kThreadNum * 2);
+  if (blk_num > 32) return 0.0f;
+  size_t smem_size = sizeof(float) * kThreadNum;
+  float *tmp;
+  CUDA_CHECK(cudaMalloc(&tmp, sizeof(float) * blk_num));
+  reduce<kThreadNum><<<blk_num, kThreadNum, smem_size>>>(g_idata, tmp, num);
+  // final reduction
+  float *res_cu;
+  CUDA_CHECK(cudaMalloc(&res_cu, sizeof(float)));
+  reduce<32><<<1, 32, sizeof(float) * 64>>>(tmp, res_cu, blk_num);
+  float res;
+  CUDA_CHECK(cudaMemcpy(&res, res_cu, sizeof(float), cudaMemcpyDeviceToHost));
+  CUDA_CHECK(cudaFree(tmp));
+  CUDA_CHECK(cudaFree(res_cu));
+  return res;
 }
 
 int main() {
@@ -40,7 +95,7 @@ int main() {
   float mean = 1.0f, std_dev = 10.0f;
   // generate random array
   vector<float> data;
-  float* data_cu;
+  float *data_cu;
   {
     CUDA_CHECK(cudaMalloc(&data_cu, sizeof(float) * num));
     curandGenerator_t gen;
@@ -56,4 +111,15 @@ int main() {
     sum += v;
   }
   cout << num << " random numbers generated, sum is " << sum << "\n";
+
+  // get device info
+  cudaDeviceProp prop;
+  int device;
+  CUDA_CHECK(cudaGetDevice(&device));
+  CUDA_CHECK(cudaGetDeviceProperties(&prop, device));
+  cout << "device info: name[" << prop.name << "] max_thd_num[" << prop.maxThreadsPerBlock << "]\n";
+
+  // sum up in gpu
+  auto sum_cu = reduce_cu<1024>(data_cu, num);
+  cout << "sum from cuda: " << sum_cu << "\n";
 }
---
+++ readme.md
@@ -0,0 +1,6 @@
+# CUDA Reduction
+Sum reduction in cuda.
+
+Ref:
+- [Optimizing Parallel Reduction in CUDA - NVIDIA](https://developer.download.nvidia.com/assets/cuda/files/reduction.pdf)
+- [cuda-samples](https://github.com/NVIDIA/cuda-samples/tree/v10.2/Samples/reduction)
